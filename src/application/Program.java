ba550add78b4537273232cb01e41fd710bb7e7d2
Joaomgsa <joaomgsa@gmail.com>
2022-10-27 19:39:48 -0300

    novas subclasses

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,20 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account acc = new Account(1001,"Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "OLX", 0.0, 500.0);
+
+
+        //Upcasting
+
+        Account acc1 =  bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob",0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
     }
 }
