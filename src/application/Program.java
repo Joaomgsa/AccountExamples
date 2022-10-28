aab7af6dce36c9c700f3b02de393ea5c953d97a0
Joaomgsa <joaomgsa@gmail.com>
2022-10-27 22:01:11 -0300

    novas subclasses

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,5 +19,17 @@ public class Program {
         //Downcasting
         BusinessAccount acc4 = (BusinessAccount) acc2;
         acc4.loan(100.0);
+
+        //BusinessAccount acc5 = (BusinessAccount) acc3;
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
