5bb29ce64fe3b1605c33f4298fe77acad5065d31
Joaomgsa <joaomgsa@gmail.com>
2022-10-27 22:38:24 -0300

    Exemplos usando @Override para implementar metodo da classe super

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -28,4 +28,8 @@ public class SavingsAccount extends Account{
     public void updateBalance(){
         balance += balance * interestRate;
     }
+    @Override
+    public void withdraw(double amount){
+        balance-=amount;
+    }
 }
