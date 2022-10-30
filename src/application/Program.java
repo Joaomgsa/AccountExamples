f177af73bbf84b224cabf1e757db823f8bec9429
Joaomgsa <joaomgsa@gmail.com>
2022-10-30 09:40:41 -0300

    Exemplo de polimorfismo

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,25 +6,17 @@ import entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
-        Account acc1 = new Account(1001,"Alex", 1000.0);
-        BusinessAccount bacc = new BusinessAccount(1002, "OLX", 0.0, 500.0);
 
-        acc1.withdraw(200.0);
+        // exemplo Polimorfismo
+        Account x = new Account(1020,"Alex", 1000.0);
+        Account y = new SavingsAccount(1002, "Mary", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
 
 
 
 
-        Account acc2 = new SavingsAccount(1002, "Bob",1000.0, 0.01 );
-
-        acc2.withdraw(200.0);
-
-        Account acc3 = new BusinessAccount(1003, "OLX", 1000.0, 2500.0);
-
-        acc3.withdraw(200.00);
-
-              System.out.println("Business Account balance : $"+acc1.getBalance());
-              System.out.println("Savings Account balance : $"+acc2.getBalance());
-              System.out.println("Business Account balance : $"+acc3.getBalance());
     }
 }
