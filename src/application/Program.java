0e2bcfabda1b8c67143ceeffd7337511fe84d170
Joaomgsa <joaomgsa@gmail.com>
2022-11-01 11:18:14 -0300

    Usando arraylist para acumular saldo e exibir

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,18 +4,36 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
 
-        // exemplo Polimorfismo
-        Account x = new Account(1020,"Alex", 1000.0);
-        Account y = new SavingsAccount(1002, "Mary", 1000.0, 0.01);
+        Locale.setDefault(Locale.US);
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        List<Account> list = new ArrayList<>();
 
+        list.add(new SavingsAccount(1001,"Alex",500.0,0.01));
+        list.add(new BusinessAccount(1002,"Mary",1500.0,100.0));
+        list.add(new SavingsAccount(1003,"Blob",500.0,0.01));
+        list.add(new BusinessAccount(1004,"Joan Jet",1500.0,300.0));
 
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
 
+            System.out.printf("Total Balance: %.2f%n", sum);
+
+        for (Account acc : list){
+            acc.deposit(10.0);
+        }
+
+        for (Account acc : list){
+            System.out.printf("Update balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
 
 
     }
